5cebe76a8da5fecfed58e3fc8de0bc5627b330ff
ryogo <ryogo987>
2025-09-07 16:04:46 +0900

    vewsの編集

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("こんにちはya-")
+
+def hobby(request):
+    return HttpResponse("My hobby is playing games.")
+def greet(request, name):
+    message = ("こんにちは",name, "さん")
+    return HttpResponse(message)
