f6c6b6f2833d14f494dae77ea3c4d78907c68854
ryogo <ryogo987>
2025-09-14 16:02:30 +0900

    テンプレート作成

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path("",views.index, name="index"),
-    path("hobby/", views.hobby, name="hobby"),
-    path("greet/<str:name>/", views.greet, name="greet"),
-
 ]
-
 
--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+# from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("こんにちはya-")
-
-def hobby(request):
-    return HttpResponse("My hobby is playing games.")
-def greet(request, name):
-    message = ("こんにちは",name, "さん")
-    return HttpResponse(message)
+    question_list = [
+             "ゲームは好きですか",
+             "Mincraftは好きですか",
+             "こんにちは",
+       ]
+    context = {
+          "question_list": question_list,
+          "is_polled":False,
+          "polled_msg": "投票してくれてありがとう",
+          "not_polled_msg": "投票してください泣",
+    }
+    return render(request, "main/index.html", context)
